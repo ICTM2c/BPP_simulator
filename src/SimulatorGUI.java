7c8ef39dc14fb6466ec7ce325827892f2c8abe9e
Hessel Berends <hesselberends11@gmail.com>
2018-05-16 13:56:56 +0200

    Initial commit

--- src/SimulatorGUI.java
+++ src/SimulatorGUI.java
@@ -3,13 +3,7 @@ import java.util.ArrayList;
 public class SimulatorGUI {
 
     public static void main(String[] args) {
-        int size;
-        ArrayList<Integer> array = new ArrayList<Integer>();
 
-        for (int i = 1; i < 10 ; i++) {
-            size = i + 20;
-            System.out.println("Product" + i + ": " + size);
-        }
     }
 
 }
